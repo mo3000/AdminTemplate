1ab6e94007f7109148d21bed0b33e5f18a8b705c
suiwenbo <mo3000@yeah.net>
2019-09-19 17:19:41 +0800

    correct cors header

--- src/app/Utils/JwtGuard.php
+++ src/app/Utils/JwtGuard.php
@@ -16,57 +16,63 @@ use Lcobucci\JWT\Token;
 
 class JwtGuard implements Guard
 {
-	use GuardHelpers;
+    use GuardHelpers;
 
-	public function user()
-	{
-		if (!empty($this->user)) {
-			return $this->user;
-		}
-		$token = $this->getTokenForRequest();
-		$user = null;
-		if (!empty($token)) {
-			$user = $this->provider->retrieveById($token->getClaim('sub'));
-		}
-		return $this->user = $user;
-	}
+    public function user()
+    {
+        if (!empty($this->user)) {
+            return $this->user;
+        }
+        $token = $this->getTokenForRequest();
+        $user = null;
+        if (!empty($token)) {
+            $user = $this->provider->retrieveById($token->getClaim('sub'));
+        }
+        return $this->user = $user;
+    }
 
-	public function attempt(array $credentials, bool $remember=false) : bool
+    public function attempt(array $credentials, bool $remember=false) : bool
     {
         $user = $this->getProvider()->retrieveByCredentials($credentials);
+        if ($user == null) {
+            return false;
+        }
         if ($this->getProvider()->validateCredentials($user, $credentials)) {
             $this->user = $user;
         }
         return $this->user != null;
     }
 
-	public function getTokenForRequest() : ? Token
-	{
-		$request = resolve(Request::class);
+    public function getTokenForRequest() : ? Token
+    {
+        $request = resolve(Request::class);
 
-		$token = $request->bearerToken();
+        $token = $request->bearerToken();
+        if (empty($token)) {
+            $token = $request->input('authtoken');
+        }
 
-		if (!empty($token)) {
-			try {
-				$jwtToken = (new Parser())->parse($token);
-				if ($jwtToken->verify(
-					(new Sha256()), config('auth.token_secret_key'))) {
-					return $jwtToken;
-				}
-			} catch (\BadMethodCallException | \InvalidArgumentException $e) {
+        if (!empty($token)) {
+            try {
+                $jwtToken = (new Parser())->parse($token);
+                if ($jwtToken->verify(
+                    (new Sha256()), config('auth.token_secret_key'))) {
+                    return $jwtToken;
+                }
+            } catch (\BadMethodCallException | \InvalidArgumentException $e) {
 
-			}
-		}
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public function validate(array $credentials = [])
-	{
-		return true;
-	}
+    public function validate(array $credentials = [])
+    {
+        return true;
+    }
 
-	public function __construct($provider)
+    public function __construct($provider)
     {
         $this->setProvider($provider);
     }

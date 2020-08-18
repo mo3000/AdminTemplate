00c526086bb73ea1ee83714f3b7b71f94d0f521b
suiwenbo <mo3000@yeah.net>
2020-08-19 05:12:59 +0800

    admin edit

--- src/app/Http/Controllers/Auth/UserController.php
+++ src/app/Http/Controllers/Auth/UserController.php
@@ -17,46 +17,76 @@ class UserController extends Controller
 {
     public function list(Request $request)
     {
-    	$list = Admin::with('roles')
-		    ->when($request->filled('realname'), function ($query) use ($request) {
-		    	$query->where('admins.realname', 'like', '%'.$request->input('realname').'%');
-		    })
-		    ->when($request->filled('username'), function ($query) use ($request) {
-			    $query->where('admins.username', 'like', '%'.$request->input('username').'%');
-		    })
-		    ->when($request->filled('rolename'), function ($query) use ($request) {
-			    $query->where('roles.display_name', 'like', '%'.$request->input('rolename').'%');
-		    })
-	        ->paginate();
-    	return new JsonResponse(0, '', $list);
+        $list = Admin::with('roles')
+            ->when($request->filled('realname'), function ($query) use ($request) {
+                $query->where('admins.realname', 'like', '%'.$request->input('realname').'%');
+            })
+            ->when($request->filled('username'), function ($query) use ($request) {
+                $query->where('admins.username', 'like', '%'.$request->input('username').'%');
+            })
+            ->when($request->filled('rolename'), function ($query) use ($request) {
+                $query->where('roles.display_name', 'like', '%'.$request->input('rolename').'%');
+            })
+            ->paginate();
+        return new JsonResponse(0, '', $list);
     }
 
     public function edit(Request $request)
     {
-    	if ($request->filled('id')) {
-    		Admin::where('id', $request->input('id'))
-			    ->update(
-			    	[
-			    		'realname' => $request->input('realname'),
-					    'updated_at' => Carbon::now()->toDateTimeString(),
-				    ]
-			    );
-	    } else {
-    		if (Admin::where('username', $request->input('username'))
-	            ->exists()) {
-    			return new JsonResponse(-1, '用户名已存在');
-		    }
-    		$admin = new Admin(
-    			[
-    				'realname' => $request->input('realname'),
-				    'username' => $request->input('username'),
-				    'password' => Hash::make(
-				    	$request->input('password', '123456')),
-			    ]
-		    );
-    		$admin->save();
-	    }
-    	return new JsonResponse(0);
+        if ($request->filled('id')) {
+            DB::beginTransaction();
+            try {
+                $admin = Admin::where('id', $request->input('id'))->first();
+                if (empty($admin)) {
+                    throw new \RuntimeException('用户不存在');
+                }
+                $admin->realname = $request->input('realname');
+                $admin->save();
+                $roles = Roles::select('id')
+                    ->whereIn('name', $request->input('roles'))
+                    ->get()
+                    ->pluck('id')
+                    ->toArray();
+                $admin->roles()->sync($roles);
+                DB::commit();
+            } catch (\RuntimeException $e) {
+                DB::rollBack();
+                return new JsonResponse(-1, $e->getMessage());
+            }
+        } else {
+            $request->validate(['username' => 'required', 'realname' => 'required', 'roles' => 'required']);
+            $password = empty($request->input('password')) ? $request->input('password') : '123456';
+            if (Admin::where('username', $request->input('username'))
+                ->exists()) {
+                return new JsonResponse(-1, '用户名已存在');
+            }
+            $admin = new Admin(
+                [
+                    'realname' => $request->input('realname'),
+                    'username' => $request->input('username'),
+                    'password' => Hash::make($password),
+                ]
+            );
+            DB::beginTransaction();
+            try {
+                $rolenames = $request->input('roles');
+                $roles = Roles::select('id')
+                    ->whereIn('name', $rolenames)
+                    ->get()
+                    ->pluck('id')
+                    ->toArray();
+                if (empty($roles)) {
+                    throw new \RuntimeException("角色不存在: ".join(', ', $rolenames));
+                }
+                $admin->save();
+                $admin->roles()->sync($roles);
+                DB::commit();
+            } catch (\RuntimeException $e) {
+                DB::rollBack();
+                return new JsonResponse(-1, $e->getMessage());
+            }
+        }
+        return new JsonResponse(0);
     }
 
     public function resetPassword(Request $request)
@@ -71,34 +101,41 @@ class UserController extends Controller
         return new JsonResponse(0);
     }
 
-    public function setStatus(Request $request)
+    public function toggleStatus(Request $request)
     {
-
+        $request->validate(['id' => 'required']);
+        if ($request->input('id') == $request->user()->id) {
+            return new JsonResponse(-1, '不能禁用自己');
+        }
+        DB::table('admins')
+            ->where('id', $request->input('id'))
+            ->update(['status' => DB::raw('status ^ 1')]);
+        return new JsonResponse(0);
     }
 
     public function info($id)
     {
-    	return Admin::find($id)->with('roles');
+        return Admin::find($id)->with('roles');
     }
 
     public function roleList(Request $request)
     {
-    	return Roles::get();
+        return Roles::get();
     }
 
     //关联角色
     public function syncUserRoles(Request $request)
     {
-    	$this->validate($request, [
-    		'id' => 'required',
-		    'roles' => 'required|array'
-	    ]);
+        $this->validate($request, [
+            'id' => 'required',
+            'roles' => 'required|array'
+        ]);
 
-    	Admin::findOrFail($request->input('id'))
-		    ->roles()
-		    ->sync($request->input('roles'));
+        Admin::findOrFail($request->input('id'))
+            ->roles()
+            ->sync($request->input('roles'));
 
-    	return new JsonResponse(0);
+        return new JsonResponse(0);
     }
 
     public function userinfo(Request $request)
